6f8204aa9464a4ddf51a0089a67c96ac605b166d
Ahmed Sleem <theyoungprogrammer67@gmail.com>
2024-10-21 10:42:02 +0300

    gradle: added test dependency in android sourceSet.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,4 +46,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    dependencies {
+        androidTestImplementation(libs.kotlin.test)
+    }
 }
