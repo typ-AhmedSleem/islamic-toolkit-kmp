aa528bb1a84fc19ba7b079c1b16e55e4e09d10eb
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-01-03 03:52:32 +0200

    ref(praytimes): updated `getJulianDate` to use `date.day`
    The `getJulianDate` method in `AstronomyUtils.kt` has been updated to use `date.day` instead of the deprecated `date.dayOfMonth` for calculating the Julian date. This aligns with the updated API.

--- src/commonMain/kotlin/com/typ/islamic_toolkit_kmp/praytimes/utils/AstronomyUtils.kt
+++ src/commonMain/kotlin/com/typ/islamic_toolkit_kmp/praytimes/utils/AstronomyUtils.kt
@@ -4,7 +4,6 @@ import com.typ.islamic_toolkit_kmp.core.annotations.NeedsTesting
 import com.typ.islamic_toolkit_kmp.core.datetime.Timestamp
 import com.typ.islamic_toolkit_kmp.praytimes.enums.HigherLatitudeMethod
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.isoDayNumber
 import kotlinx.datetime.number
 import kotlin.math.floor
 
@@ -33,7 +32,7 @@ object AstronomyUtils {
         val A = floor(tYear / 100.0)
         val B = 2 - A + floor(A / 4.0)
 
-        return floor(365.25 * (tYear + 4716)) + floor(30.6001 * (tMonth + 1)) + date.dayOfMonth + B - 1524.5
+        return floor(365.25 * (tYear + 4716)) + floor(30.6001 * (tMonth + 1)) + date.day + B - 1524.5
     }
 
     /** Convert a calendar date to julian date (second method) */
